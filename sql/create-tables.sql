fe7ac7bd0d3f97dd0b634fe0e208f56c5fd5cee4
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-12 06:02:29 -0500

    add completed to Items database table

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,36 +1,37 @@
-DROP TABLE IF EXISTS
-    Items,
-    Checklists,
-    Users;
+DROP TABLE IF EXISTS Items, Checklists, Users;
 
 CREATE TABLE Users (
-    id INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    email CHAR(50) NOT NULL UNIQUE,
-    name_first CHAR(40) NOT NULL,
-    name_last CHAR(60) NOT NULL,
-    password CHAR(255) NOT NULL,
+    id           INT UNSIGNED NOT NULL UNIQUE auto_increment,
+    email        CHAR(50) NOT NULL UNIQUE,
+    name_first   CHAR(40) NOT NULL,
+    name_last    CHAR(60) NOT NULL,
+    password     CHAR(255) NOT NULL,
     date_created DATETIME NOT NULL,
     PRIMARY KEY (id)
-) Engine = InnoDB;
+)
+engine = innodb;
 
 CREATE TABLE Checklists (
-    id INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    name CHAR(100) NOT NULL,
-    date_created DATETIME NOT NULL,
+    id            INT UNSIGNED NOT NULL UNIQUE auto_increment,
+    user_id       INT UNSIGNED NOT NULL,
+    name          CHAR(100) NOT NULL,
+    date_created  DATETIME NOT NULL,
     date_modified DATETIME,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES Users(id) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB;
+)
+engine=innodb;
 
 CREATE TABLE Items (
-    id INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    checklist_id INT UNSIGNED NOT NULL,
-    content CHAR(250),
-    date_created DATETIME NOT NULL,
+    id            INT UNSIGNED NOT NULL UNIQUE auto_increment,
+    checklist_id  INT UNSIGNED NOT NULL,
+    content       CHAR(250),
+    completed     ENUM ('y', 'n') NOT NULL DEFAULT 'n',
+    date_created  DATETIME NOT NULL,
     date_modified DATETIME,
-    rank DECIMAL (7,7) NOT NULL,
+    rank          DECIMAL (7, 7) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (checklist_id) REFERENCES Checklists(id) ON UPDATE CASCADE ON DELETE CASCADE
-) Engine = InnoDB;
+)
+engine = innodb; 
 

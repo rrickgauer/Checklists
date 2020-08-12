c42f5c7ebb12d77573647c698f37738a7fa70082
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-11 21:56:46 -0500

    add login page

---
+++ src/login.php
@@ -0,0 +1,38 @@
+<?php include('functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include('header.php'); ?>
+  <title>Login</title>
+</head>
+<body>
+  <div class="container">
+
+    <h1 class="text-center mt-5 mb-5">Login to checklists</h1>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+<?php include('footer.php'); ?>
+</body>
+</html>

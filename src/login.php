de01f18cd5a76f2028a407729ac91e552211ae17
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-11 23:35:57 -0500

    clear the session data every time

--- src/login.php
+++ src/login.php
@@ -1,4 +1,12 @@
-<?php include('functions.php'); ?>
+<?php
+
+// clear session data
+session_start();
+session_destroy();
+$_SESSION = array();
+
+include('functions.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,13 +18,12 @@
 
     <h1 class="text-center mt-5 mb-5">Login to checklists</h1>
     
-
-
+    <!-- create new account form -->
     <h4>Create account</h4>
     <form class="form-create-account" method="post" action="api.checklists.php">
       <!-- email -->
       <div class="form-group">
-        <label>Email address</label>
+        <label>Email</label>
         <div class="input-group">
           <div class="input-group-prepend">
             <span class="input-group-text"><i class='bx bx-envelope'></i></span>
@@ -62,21 +69,33 @@
     </form>
 
 
+    <!-- login form -->
+    <h4 class="mt-5">Login to your account</h4>
+    <form class="form-login" method="post" action="api.checklists.php">
+      <!-- email -->
+      <div class="form-group">
+        <label>Email</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class='bx bx-envelope'></i></span>
+          </div>
+          <input type="email" class="form-control" name="login-email" required>
+        </div>
+      </div>
 
+      <!-- password -->
+      <div class="form-group">
+        <label>Password</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class='bx bx-lock-alt'></i></span>
+          </div>
+          <input type="password" class="form-control" name="login-password" required>
+        </div>
+      </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <input type="submit" class="btn btn-primary" value="Log in">
+    </form>
   </div>
 
 

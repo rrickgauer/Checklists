9f2d57bd2c2badc00c7bd29c53825b4a1ef69a0b
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-11 22:46:10 -0500

    add insertUser()

--- src/functions.php
+++ src/functions.php
@@ -15,4 +15,34 @@ function dbConnect() {
       return 0;
   }
 }
+
+
+
+// create new account
+function insertUser($email, $password, $firstName, $lastName) {
+  $stmt = 'INSERT INTO Users (email, name_first, name_last, password, date_created) VALUES (:email, :firstName, :lastName, :password, NOW())';
+
+  $sql = dbConnect()->prepare($stmt);
+
+  // email
+  $email = filter_var($email, FILTER_SANITIZE_STRING);
+  $sql->bindParam(':email', $email, PDO::PARAM_STR);
+
+  // first name
+  $firstName = filter_var($firstName, FILTER_SANITIZE_STRING);
+  $sql->bindParam(':firstName', $firstName, PDO::PARAM_STR);
+
+  // last name
+  $lastName = filter_var($lastName, FILTER_SANITIZE_STRING);
+  $sql->bindParam(':lastName', $lastName, PDO::PARAM_STR);
+
+  // password
+  $password = filter_var($password, FILTER_SANITIZE_STRING);
+  $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+  $sql->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
+
+  $sql->execute();
+  return $sql;
+}
+
 ?>

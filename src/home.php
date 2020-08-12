63bba97580da2828d60213bb2ee03150a7a2e6cb
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-12 08:21:21 -0500

    add sidebar html, make code that displays alert after a checklist was created into a function

--- src/home.php
+++ src/home.php
@@ -20,56 +20,61 @@ $user = getUser($_SESSION['userID'])->fetch(PDO::FETCH_ASSOC);  // get user data
 <body>
   <?php include('navbar.php'); ?>
 
-  <div class="container">
-    <h1 class="text-center mt-5">Welcome <?php echo $user['name_first']; ?></h1>
+  <div class="wrapper">
+    <!-- checklist sidebar -->
+    <div class="sidebar active">
+      <div class="list-group">
+        <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
+        <button type="button" class="list-group-item">Morbi leo risus</button>
+        <button type="button" class="list-group-item">Porta ac consectetur ac</button>
+        <button type="button" class="list-group-item">Vestibulum at eros</button>
+      </div>
+    </div>
 
-    <?php
-      // display alert if user attempted to create a checklist
-    if (isset($_SESSION['checklist-created'])) {
-      $created = $_SESSION['checklist-created'];
-      if ($created == true)
-        echo getAlert('Checklist created successfully.');
-      else
-        echo getAlert('Error when attempting to create checklist.', 'danger');
+    <!-- open checklists -->
+    <div class="content">
+      <div class="container-fluid">
+        <h1 class="text-center">Welcome <?php echo $user['name_first']; ?></h1>
+        <?php displayChecklistCreated() ?>
 
-        // clear out session variable
-      unset($_SESSION['checklist-created']);
-    }
-    ?>
+        <button type="button" class="btn btn-secondary btn-toggle-sidebar">Toggle sidebar</button>
 
-    <!-- new checklist modal trigger button -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-new-checklist">
-      New checklist
-    </button>
+        <!-- new checklist modal trigger button -->
+        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#modal-new-checklist">
+          New checklist
+        </button>
 
-    <!-- new checklist modal -->
-    <div class="modal fade" id="modal-new-checklist" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">New Checklist</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <!-- new checklist form -->
-            <form class="form-new-checklist" method="post" action="api.checklists.php">
-              <!-- name -->
-              <div class="form-group">
-                <label>Name</label>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class='bx bx-list-check'></i></span>
-                  </div>
-                  <input type="text" class="form-control" name="new-checklist-name" required>
-                </div>
+        <!-- new checklist modal -->
+        <div class="modal fade" id="modal-new-checklist" tabindex="-1">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">New Checklist</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </div>
+              <div class="modal-body">
+                <!-- new checklist form -->
+                <form class="form-new-checklist" method="post" action="api.checklists.php">
+                  <!-- name -->
+                  <div class="form-group">
+                    <label>Name</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class='bx bx-list-check'></i></span>
+                      </div>
+                      <input type="text" class="form-control" name="new-checklist-name" required>
+                    </div>
+                  </div>
 
-              <input type="submit" class="btn btn-primary float-right" value="Save checklist">
-            </form>
+                  <input type="submit" class="btn btn-primary float-right" value="Save checklist">
+                </form>
+              </div>
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -80,3 +85,27 @@ $user = getUser($_SESSION['userID'])->fetch(PDO::FETCH_ASSOC);  // get user data
 
 </body>
 </html>
+
+
+<?php
+
+// functions
+
+// display whether or not checklist was created
+function displayChecklistCreated() {
+  // display alert if user attempted to create a checklist
+  if (isset($_SESSION['checklist-created'])) {
+    $created = $_SESSION['checklist-created'];
+    
+    if ($created == true)
+      echo getAlert('Checklist created successfully.');
+    else
+      echo getAlert('Error when attempting to create checklist.', 'danger');
+
+      // clear out session variable
+    unset($_SESSION['checklist-created']);
+  }
+}
+
+
+?>

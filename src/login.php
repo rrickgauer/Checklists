1c7cfc33bdb54f4e2ba5a909d3030a8e35504a27
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-11 23:01:01 -0500

    add create account form

--- src/login.php
+++ src/login.php
@@ -12,7 +12,54 @@
     
 
 
+    <h4>Create account</h4>
+    <form class="form-create-account" method="post" action="api.checklists.php">
+      <!-- email -->
+      <div class="form-group">
+        <label>Email address</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class='bx bx-envelope'></i></span>
+          </div>
+          <input type="email" class="form-control" name="new-email" required>
+        </div>
+      </div>
 
+      <!-- first name -->
+      <div class="form-group">
+        <label>First name</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class='bx bx-user'></i></span>
+          </div>
+          <input type="text" class="form-control" name="new-name-first" required>
+        </div>
+      </div>
+
+      <!-- last name -->
+      <div class="form-group">
+        <label>Last name</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class='bx bx-user'></i></span>
+          </div>
+          <input type="text" class="form-control" name="new-name-last" required>
+        </div>
+      </div>
+
+      <!-- password -->
+      <div class="form-group">
+        <label>Password</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class='bx bx-lock-alt'></i></span>
+          </div>
+          <input type="password" class="form-control" name="new-password" required>
+        </div>
+      </div>
+
+      <input type="submit" class="btn btn-primary" value="Create account">
+    </form>
 
 
 

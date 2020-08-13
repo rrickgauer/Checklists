0a2e29277899adf834328b74d7f94f18a6744a02
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-13 11:54:21 -0500

    load the checklist sidebar on initial page load

--- src/js/home-js.js
+++ src/js/home-js.js
@@ -1,7 +1,9 @@
+const API = 'api.checklists.php';
 
 // main function
 $(document).ready(function() {
   addEventListeners();
+  getChecklists();
 });
 
 function addEventListeners() {
@@ -11,3 +13,43 @@ function addEventListeners() {
 function toggleSidebar() {
   $('.sidebar').toggleClass('active');
 }
+
+
+// get the checklists from the server
+function getChecklists() {
+  var data = {
+    function: 'get-checklists',
+  }
+
+  $.get(API, data, function(response) {
+    displayChecklists(JSON.parse(response));
+  });
+}
+
+// display the user checklists
+function displayChecklists(checklists) {
+  const size = checklists.length;
+  var html = '';
+
+  // load html
+  for (var count = 0; count < size; count++) 
+    html += getChecklistSidebarHtml(checklists[count]);
+  
+
+  // display html
+  $(".sidebar .list-group").html(html);
+}
+
+// generates and returns the sidebar checklist html
+function getChecklistSidebarHtml(checklist) {
+  var html = '';
+  html += '<button type="button" class="list-group-item" data-checklist-id="' + checklist.id + '">';
+  html += checklist.name + '</button>';
+
+  return html;
+}
+
+
+
+
+

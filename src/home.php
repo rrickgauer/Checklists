53cbfcebf3ba03ca13fc58ddfde36fa9c47ae7ea
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-17 12:41:48 -0500

    remove commented code

--- src/home.php
+++ src/home.php
@@ -37,8 +37,7 @@ $user = getUser($_SESSION['userID'])->fetch(PDO::FETCH_ASSOC);  // get user data
           <h1 class="text-center">Welcome <?php echo $user['name_first']; ?></h1>
           <?php displayChecklistCreated() ?>
 
-          <!-- <button type="button" class="btn btn-secondary btn-toggle-sidebar">Toggle sidebar</button> -->
-
+          <!-- toggle sidebar -->
           <button class="hamburger hamburger--elastic is-active btn-toggle-sidebar" type="button">
             <span class="hamburger-box">
               <span class="hamburger-inner"></span>

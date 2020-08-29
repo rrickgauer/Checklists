92efd5cb0ee5cc7095c149c9cf373841918928c9
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-08-29 10:47:29 -0500

    include jquery toast library

--- src/header.php
+++ src/header.php
@@ -12,5 +12,8 @@
 <!-- animate.css -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
 
+<!-- jquery toast -->
+<link rel="stylesheet" type="text/css" href="css/jquery.toast.min.css" />
+
 <!-- personal css -->
 <link rel="stylesheet" type="text/css" href="css/style.css" />

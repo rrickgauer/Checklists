4c1bd417f7ddbc13a7b07be271839e085a0ac54a
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-09-14 20:48:40 -0500

    add link to github

--- src/navbar.php
+++ src/navbar.php
@@ -4,11 +4,21 @@
       <i class='bx bx-menu'></i>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse navbar-items" id="navbarNavDropdown">
+      <ul class="navbar-nav left">
         <li class="nav-item"><a class="nav-link" id="nav-item-home" href="home.php">Home</a></li>
         <li class="nav-item"><a class="nav-link" id="nav-item-settings" href="settings.php">Settings</a></li>
+        
+      </ul>
+
+      <ul class="navbar-nav right">
         <li class="nav-item"><a class="nav-link" id="nav-item-logout" href="login.php">Logout</a></li>
+        <!-- github link -->
+        <li class="nav-item">
+          <a class="nav-link" id="nav-item-github" href="https://github.com/rrickgauer/Checklists" target="_blank">
+            <i class='bx bxl-github'></i>
+          </a>
+        </li>
       </ul>
     </div>
   </nav>
